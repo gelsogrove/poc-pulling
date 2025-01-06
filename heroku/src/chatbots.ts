ff8fe844969cacae87dba0a4e795a295c2f8b5fc
Andrea <gelsogrove@gmail.com>
2025-01-06 01:15:25 +0100

    deploy

--- heroku/src/chatbots.ts
+++ heroku/src/chatbots.ts
@@ -7,8 +7,8 @@ import { getUserIdByToken } from "./validateUser.js"
 dotenv.config() // Carica le variabili d'ambiente
 
 const OPENROUTER_API_URL = "https://openrouter.ai/api/v1/chat/completions"
-//const OPENROUTER_MODEL = "openai/gpt-3.5-turbo"
-const OPENROUTER_MODEL = "google/gemini-2.0-flash-exp:free"
+const OPENROUTER_MODEL = "openai/gpt-3.5-turbo"
+//const OPENROUTER_MODEL = "google/gemini-2.0-flash-exp:free"
 
 const OPENROUTER_HEADERS = {
   Authorization: `Bearer ${process.env.OPENROUTER_API_KEY}`,
