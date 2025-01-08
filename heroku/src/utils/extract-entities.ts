faaec3abe7982f70dd9ecded9402cb682f784867
Andrea <gelsogrove@gmail.com>
2025-01-08 14:10:53 +0100

    deploy

--- heroku/src/utils/extract-entities.ts
+++ heroku/src/utils/extract-entities.ts
@@ -1,27 +1,30 @@
-// Mappa dei dati sensibili e dei rispettivi token
-const sensitiveDataMap: { [key: string]: string } = {
-  "Andrea Gelsomino": "TOKEN_0001",
-  Milano: "TOKEN_0001",
-}
+// Array dei dati sensibili con rispettivi token
+const sensitiveDataArray: { key: string; value: string }[] = [
+  { key: "Andrea Gelsomino", value: "TOKEN_0001" },
+  { key: "Milano", value: "TOKEN_0002" },
+]
 
 // Funzione per tokenizzare (sostituire i dati sensibili con i rispettivi token)
 export function tokenize(inputString: string, conversationId: string): string {
   // Aggiungiamo conversationId al token per personalizzare
-  const personalizedSensitiveDataMap = { ...sensitiveDataMap }
-  Object.keys(personalizedSensitiveDataMap).forEach((key) => {
-    personalizedSensitiveDataMap[
-      key.toLowerCase() // Convertiamo la chiave in lowercase per il confronto
-    ] = `${personalizedSensitiveDataMap[key]}_${conversationId}`
-  })
+  const personalizedSensitiveDataArray = sensitiveDataArray.map((item) => ({
+    key: item.key.toLowerCase(), // Convertiamo la chiave in lowercase per il confronto
+    value: `${item.value}_${conversationId}`, // Aggiungiamo conversationId
+  }))
 
   // Creiamo una regex con case-insensitivity
   const regex = new RegExp(
-    Object.keys(personalizedSensitiveDataMap).map(escapeRegex).join("|"),
+    personalizedSensitiveDataArray
+      .map((item) => escapeRegex(item.key))
+      .join("|"),
     "gi" // 'g' per sostituire globalmente, 'i' per ignorare maiuscole/minuscole
   )
 
   return inputString.replace(regex, (match) => {
-    return personalizedSensitiveDataMap[match.toLowerCase()] // Confrontiamo in lowercase
+    const found = personalizedSensitiveDataArray.find(
+      (item) => item.key === match.toLowerCase()
+    )
+    return found ? found.value : match
   })
 }
 
@@ -32,19 +35,20 @@ export function untokenize(
 ): string {
   // Rimuoviamo il conversationId dal token
   const reverseMap = Object.fromEntries(
-    Object.entries(sensitiveDataMap).map(([key, value]) => {
-      return [`${value}_${conversationId}`, key] // Manteniamo le chiavi nella forma originale
-    })
+    sensitiveDataArray.map((item) => [
+      `${item.value}_${conversationId}`,
+      item.key, // Manteniamo le chiavi nella forma originale
+    ])
   )
 
   // Creiamo una regex per i token
   const regex = new RegExp(
     Object.keys(reverseMap).map(escapeRegex).join("|"),
-    "gi" // 'g' per sostituire globalmente, 'i' per ignorare maiuscole/minuscole
+    "g" // 'g' per sostituire globalmente
   )
 
   return inputString.replace(regex, (match) => {
-    return reverseMap[match] // Non serve toLowerCase qui perché i token sono esatti
+    return reverseMap[match] // I token sono esatti
   })
 }
 
