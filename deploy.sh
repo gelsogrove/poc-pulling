92b83978b5d96ede24a62b5efb8b2b8813b14bd3
Andrea <gelsogrove@gmail.com>
2025-01-10 19:41:35 +0100

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
+set -x # Attiva il debug
 
-# 1. Aggiungi e spingi i cambiamenti su Git
+# Git push
 git add .
 git commit -m "deploy"
 git push
 
-
-# 3. Esegui il build del progetto con npm
+# Esegui build
 echo 'Running npm build...'
 cd daily-milk/frontend || exit 1
 npm install
 npm run build || { echo "Build failed"; exit 1; }
 cd ../../
 
-# 4. Carica i file con SCP usando sshpass
+# SCP per caricare i file
 echo 'Uploading files to server...'
 sshpass -p 'Almogavers@123' scp -P 22 -r daily-milk/frontend/build/* u94121824@home744730785.1and1-data.host:/ai/
+set +x # Disattiva il debug
+
 
 
 # 2. Mostra i log di Heroku
