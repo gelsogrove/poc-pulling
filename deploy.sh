20469ebd0233e668c02e7bbbf6527377e47a73f0
Andrea <gelsogrove@gmail.com>
2025-01-11 23:30:27 +0100

    deploy

--- deploy.sh
+++ deploy.sh
@@ -13,15 +13,9 @@ npm install
 npm run build || { echo "Build failed"; exit 1; }
 cd ../../
 
-# SCP per caricare i file
-echo 'Uploading files to server...'
-sshpass -p 'Almogavers@123' scp -P 22 -r daily-milk/frontend/build/* u94121824@home744730785.1and1-data.host:/ai/
-set +x # Disattiva il debug
-
-
-
-# 2. Mostra i log di Heroku
-# echo '*********'
-# heroku logs --tail --app poulin
+ 
+ 2. Mostra i log di Heroku
+ echo '*********'
+ heroku logs --tail --app poulin
 
 
