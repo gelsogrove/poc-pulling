5ba97fcae87f5a74eada9c274529714c45d980f4
Andrea <gelsogrove@gmail.com>
2025-01-24 13:48:18 +0100

    test

--- daily-milk/frontend/src/www/components/popups/prompts/salesreader/api/PromptsApi.js
+++ daily-milk/frontend/src/www/components/popups/prompts/salesreader/api/PromptsApi.js
@@ -4,7 +4,12 @@ const API_URL = `${process.env.REACT_APP_API_URL}/prompt`
 
 export const getPrompt = async (idPrompt, token) => {
   try {
-    const response = await axios.post(API_URL, { idPrompt, token })
+    const headers = {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    }
+
+    const response = await axios.post(API_URL, { idPrompt, token }, headers)
     console.log("Prompt fetched:", response.data.content)
     return response.data.content // Restituisce introduction, model, e temperature
   } catch (error) {
--- daily-milk/frontend/src/www/pages/login/api/isExpireApi.js
+++ daily-milk/frontend/src/www/pages/login/api/isExpireApi.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 import Cookies from "js-cookie"
 
-const API_URL = `${process.env.REACT_APP_API_URL}/auth/is-expired`
+export const API_URL = `${process.env.REACT_APP_API_URL}/auth/is-expired`
 
 export const IsExpired = async () => {
   const token = Cookies.get("token") // Ottieni il token dai cookie
