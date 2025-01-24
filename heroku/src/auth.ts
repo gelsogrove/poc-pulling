301e2374d81ca40d0da2477f8287ff62a96dff95
Andrea <gelsogrove@gmail.com>
2025-01-24 13:34:07 +0100

    test auth

--- heroku/src/auth.ts
+++ heroku/src/auth.ts
@@ -155,7 +155,7 @@ const setExpire: RequestHandler = async (req, res) => {
   const { userId } = req.body
 
   // Calcola la nuova data di scadenza (30 minuti da ora)
-  const newExpireDate = new Date(Date.now() + 59 * 60 * 1000) // Aggiungi 30 minuti
+  const newExpireDate = new Date(Date.now() + 120 * 60 * 1000) // Aggiungi 120 minuti
 
   // Genera un nuovo token
   const token = uuidv4() // Genera un token unico
