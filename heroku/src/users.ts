9dcc893e5939d101a255d1b85a1ecf23347bf92e
Andrea <gelsogrove@gmail.com>
2025-01-29 23:30:19 +0100

    test

--- heroku/src/users.ts
+++ heroku/src/users.ts
@@ -39,8 +39,8 @@ usersRouter.put("/update/:id", async (req: Request, res: Response) => {
   }
   try {
     const query =
-      "UPDATE users SET name = $1, role = $2, username = $3, surname = $4, isactive = $5, isActive = $6 WHERE userid = $7 RETURNING *"
-    const values = [name, role, username, surname, active, isActive, id]
+      "UPDATE users SET name = $1, role = $2, username = $3, surname = $4, isactive = $5 WHERE userid = $7 RETURNING *"
+    const values = [name, role, username, surname, isActive, id]
     const result = await pool.query(query, values)
     if (result.rowCount === 0) {
       res.status(404).json({ error: "User not found." })
