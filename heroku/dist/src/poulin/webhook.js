83cbc18f03df2f8a12f75c275e979234cfdeefd6
Andrea <gelsogrove@gmail.com>
2025-03-10 23:47:16 +0100

    Aggiunto parametro action al messaggio interattivo di WhatsApp

--- heroku/dist/src/poulin/webhook.js
+++ heroku/dist/src/poulin/webhook.js
@@ -132,6 +132,24 @@ async function sendWelcomeMessage(to, name) {
                 body: {
                     text: `Ciao *${name}*, mi chiamo Eva e sono un assistente virtuale dell'Altra Italia (https://laltrait.com/), sono un assistenza di primo livello se non riusciamo a darti l'informazione che cerchi ti metteremo in contatto con un operatore.\n\nCome ti posso aiutare oggi?`,
                 },
+                action: {
+                    buttons: [
+                        {
+                            type: "reply",
+                            reply: {
+                                id: "btn-info",
+                                title: "Informazioni",
+                            },
+                        },
+                        {
+                            type: "reply",
+                            reply: {
+                                id: "btn-help",
+                                title: "Aiuto",
+                            },
+                        },
+                    ],
+                },
             },
         }, {
             headers: {
